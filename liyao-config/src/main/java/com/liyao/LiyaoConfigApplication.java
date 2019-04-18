5448f9933f1b3607aaf68826561d0e455f255686
weiqicheng <1403773936@qq.com>
2019-04-18 23:02:10 +0800

    魏起成

--- liyao-config/src/main/java/com/liyao/LiyaoConfigApplication.java
+++ liyao-config/src/main/java/com/liyao/LiyaoConfigApplication.java
@@ -3,12 +3,14 @@ package com.liyao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class LiyaoConfigApplication {
 
     public static void main(String[] args) {
